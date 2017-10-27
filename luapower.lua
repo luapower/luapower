5fcaa536b9507cf74bf3cd83d89988ee7dd51508
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2017-10-27 18:43:27 +0300

    removed CI server functions

--- luapower.lua
+++ luapower.lua
@@ -2012,34 +2012,4 @@ function restart() error'not connected' end
 function stop() error'not connected' end
 
 
---luapower CI server
---============================================================================
-
-local function parse(s)
-	local f, err = loadstring('return '..s)
-	if not f then return nil, err end
-	return glue.protect(f)()
-end
-
-local function format(t)
-	return pp.format(t)
-end
-
-local function send(skt, t)
-	local s = format(t)
-	return skt:send(#s..'\n'..s)
-end
-
-local function receive(skt)
-	local s, err = skt:receive'*l'
-	local sz = tonumber(s)
-	if not sz then
-		return nil,
-			'message size expected, got '..tostring(s)..', '..tostring(err)
-		end
-	local s = skt:receive(sz)
-	if not s then return nil, sz..' bytes message expected' end
-	return parse(s)
-end
-
 return luapower
